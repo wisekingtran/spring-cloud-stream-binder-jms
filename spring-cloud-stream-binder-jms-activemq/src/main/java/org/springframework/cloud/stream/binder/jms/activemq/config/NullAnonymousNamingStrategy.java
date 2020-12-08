a3301a5e61e67ab3f4a70255a8f3e5ff6ab2117d
Minh Tran <thminh_1@spsvietnam.vn>
2020-12-08 10:07:11 +0700

    Upgrade to support Spring Cloud Hoxton and Spring Cloud Stream Horsham.SR10

--- spring-cloud-stream-binder-jms-activemq/src/main/java/org/springframework/cloud/stream/binder/jms/activemq/config/NullAnonymousNamingStrategy.java
+++ spring-cloud-stream-binder-jms-activemq/src/main/java/org/springframework/cloud/stream/binder/jms/activemq/config/NullAnonymousNamingStrategy.java
@@ -2,7 +2,7 @@ package org.springframework.cloud.stream.binder.jms.activemq.config;
 
 import org.springframework.cloud.stream.binder.jms.utils.AnonymousNamingStrategy;
 
-public class NullAnonymousNamingStrategy implements AnonymousNamingStrategy{
+public class NullAnonymousNamingStrategy implements AnonymousNamingStrategy {
 
     @Override
     public String generateName() {
@@ -10,7 +10,7 @@ public class NullAnonymousNamingStrategy implements AnonymousNamingStrategy{
     }
 
     @Override
-    public String generateName(String prefix) {
+    public String generateName(final String prefix) {
         return null;
     }
 
